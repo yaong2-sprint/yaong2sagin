6014fb10cd5330cd762cbc9d1e3fe03ca5bca413
LeeYeonJu <yeonzzu2@gmail.com>
2021-06-23 21:39:22 +0900

    env : babel, file loader, devServer를 webpack에 설정한다

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,53 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
+const prod = process.env.NODE_ENV === 'production';
 module.exports = {
-  mode: 'development', // 'production' or "development" or "none"
+  // webpack은 mode에 따라 내장 최적화
+  mode: prod ? 'production' : 'development', // 'production' or "development" or "none"
+  // 소스 맵 생성 여부 및 방법 설정
+  devtool: prod ? 'none' : 'eval-source-map', // https://brightparagon.wordpress.com/2018/06/27/webpack-v4-development-configuration/
   entry: './src/main.js',
   output: {
-    filename: 'main.js',
+    filename: 'static/js/main.js',
     path: path.resolve(__dirname, 'public'),
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({ template: `./index.html` }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'static/images',
+            },
+          },
+        ],
+      },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 8080,
+    hot: true,
+    publicPath: '/',
   },
 };
