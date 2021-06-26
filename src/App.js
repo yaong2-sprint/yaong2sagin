8044a71215b524b637f125d5e20f5298860d2dd5
LeeYeonJu <yeonzzu2@gmail.com>
2021-06-27 05:43:50 +0900

    feat : sleep함수를 만들어 로딩UI를 테스트하고 컴포넌트 인자를 객체형태로 바꾼다

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Slider from '@components/Slider';
 import SearchBar from '@components/SearchBar';
 
 export default class App {
-  constructor($target) {
+  constructor({ $target }) {
     this.$target = $target;
+
+    this.slider = new Slider({ $target });
     this.loader = new Loader({
       $target,
     });
-    this.slider = new Slider({ $target });
-    this.searchSection = new SearchBar({ $target });
+
+    this.sleep = (ms) => new Promise((r) => setTimeout(r, ms)); // 로더 테스트용
+    this.searchSection = new SearchBar({
+      $target,
+      onSearch: async (keyword, isRandom) => {
+        this.loader.toggleLoader();
+        console.log(keyword, isRandom);
+        await this.sleep(3000); // TODO : 3초 뒤 로더 사라짐 -> api 호출
+        this.loader.toggleLoader();
+      },
+    });
     // this.resultSection = new ResultSection($target, initialData);
   }
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,9 +2,12 @@ import SearchInput from './SearchInput';
 import RandomSearchButton from './RandomSearchButton';
 import SearchHistory from './SearchHistory';
 
-export default class Search {
-  constructor({ $target }) {
+export default class SearchBar {
+  constructor({ $target, onSearch }) {
     this.$target = $target;
+    this.onSearch = onSearch;
+
+    this.keywords = JSON.parse(sessionStorage.getItem('keywords'));
 
     this.$searchSection = document.createElement('section');
     this.$searchSection.className = 'search-section';
@@ -12,16 +15,27 @@ export default class Search {
     this.render();
   }
 
+  addKeyword(keyword) {
+    this.keywords = [keyword, ...this.keywords];
+  }
+
+  removeKeyword(keyword) {
+    this.keywords = [keyword, ...this.keywords];
+  }
+
   render() {
     this.$searchInputWrapper = document.createElement('div');
     this.$searchInputWrapper.className = 'search-input-wrapper';
     this.inputComponents = [
-      new SearchInput(this.$searchInputWrapper),
-      new RandomSearchButton(this.$searchInputWrapper),
+      new SearchInput({ $target: this.$searchInputWrapper }),
+      new RandomSearchButton({
+        $target: this.$searchInputWrapper,
+        onSearch: this.onSearch,
+      }),
     ];
     this.$searchSection.appendChild(this.$searchInputWrapper);
 
-    this.$searchHistory = new SearchHistory(this.$searchSection);
+    this.$searchHistory = new SearchHistory({ $target: this.$searchSection });
 
     this.$target.append(this.$searchSection);
   }
