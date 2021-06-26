3e6800f3069d5f041f53d4fb2fc283779779a5e4
LeeYeonJu <yeonzzu2@gmail.com>
2021-06-26 21:32:51 +0900

    feat : 고양이 검색입력, 랜덤버튼, 히스토리 구성 및 css 작업하였다

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Search from './components/Search';
+import Slider from '@components/Slider';
+import SearchBar from '@components/SearchBar';
 
 export default class App {
   constructor($target) {
-    this.target = $target;
-    this.searchSection = new Search($target);
+    this.$target = $target;
+    this.$Slider = new Slider($target);
+    this.$searchSection = new SearchBar($target);
     // this.resultSection = new ResultSection($target, initialData);
   }
 }
--- src/components/SearchBar/SearchInput.js
+++ src/components/SearchBar/SearchInput.js
@@ -3,6 +3,7 @@ export default class SearchInput {
     this.searchInput = document.createElement('input');
     this.searchInput.className = 'search-input';
     this.searchInput.placeholder = '고양이를 찾아봅시다';
+    this.searchInput.autofocus = true;
 
     $target.appendChild(this.searchInput);
   }
